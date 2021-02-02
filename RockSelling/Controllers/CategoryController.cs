9649b020f3a4c6b53ad97abb97cad67497ce4959
Maharoz <mahruj66@gmail.com>
2021-02-02 23:39:01 +0600

    edit button on category added

--- RockSelling/Controllers/CategoryController.cs
+++ RockSelling/Controllers/CategoryController.cs
@@ -30,6 +30,8 @@ namespace RockSelling.Controllers
             return View();
         }
 
+       
+
         //Post-Create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -44,6 +46,25 @@ namespace RockSelling.Controllers
             }
             return View(obj);
            
+        }
+
+
+        //Get-Edit
+
+        public IActionResult Edit(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
