427bf27f37eecad40c6be3dc498e18f814a73a73
Maharoz <mahruj66@gmail.com>
2021-02-03 13:15:23 +0600

    product view added

---
+++ RockSelling/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockSelling.Data;
+using RockSelling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockSelling.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public ProductController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach(var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u=>u.Id == obj.CategoryId);
+            }
+            return View(objList);
+        }
+
+
+        //Get-Upsert
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+            if (id == null)
+            {
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if(product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+        }
+
+
+
+        //Post-Upsert
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Product.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+           
+        }
+
+
+
+        //Get-Delete
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Product.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        //Post-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Product.Find(id);
+            
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+                _db.Product.Remove(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+           
+
+        }
+    }
+}
