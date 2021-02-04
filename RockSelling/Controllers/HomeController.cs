c83d09edbf055d19083e912104ffc3fe7c01fb91
Maharoz <mahruj66@gmail.com>
2021-02-04 16:07:21 +0600

    add to cart and remove to cart functionality added

--- RockSelling/Controllers/HomeController.cs
+++ RockSelling/Controllers/HomeController.cs
@@ -79,6 +79,29 @@ namespace RockSelling.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult RemoveFromCart(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            var itemToRemove = shoppingCartList.SingleOrDefault(r => r.ProductId == id);
+
+            if(itemToRemove != null)
+            {
+                shoppingCartList.Remove(itemToRemove);
+            }
+          //  shoppingCartList.Add(new ShoppingCart
+          //  {
+           //     ProductId = id
+           // });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Privacy()
         {
             return View();
