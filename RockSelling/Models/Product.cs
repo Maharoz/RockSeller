004f5dae95620b79639fdc1c969844581099d98d
Maharoz <mahruj66@gmail.com>
2021-02-04 12:42:19 +0600

    Modified product model

--- RockSelling/Models/Product.cs
+++ RockSelling/Models/Product.cs
@@ -14,8 +14,9 @@ namespace RockSelling.Models
 
         [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string ShortDesc { get; set; }
 
+        public string Description { get; set; }
         [Range(1,int.MaxValue)]
         public double Price { get; set; }
         public string Image { get; set; }
